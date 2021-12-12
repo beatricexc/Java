2454f742f7c7d162dd2e05809af12ece960186ca
beatricx <72341578+beatricexc@users.noreply.github.com>
2021-12-12 22:42:33 +0000

    Add files via upload

---
+++ class Main{.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        int num1 = 45;
+        int num2 = 20;
+        System.out.println(num1+num2); //addition
+        Sytem.out.println(num1-num2);  // subtraction
+        System.out.println(num1*num2); // multiplication
+        System.out.println(num1/num2); // division
+        Sytem.out.println(num2%num2); // modulo
+    }
+}
