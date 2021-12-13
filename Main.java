ebe324402ce623f3e0d1a04d0145abfed9fef5df
beatricx <72341578+beatricexc@users.noreply.github.com>
2021-12-13 21:25:24 +0000

    Area calculator

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the radius");
+    double radius = input.nextDouble();
+    if (radius >= 0) {
+      double area = radius * radius * 3.142;
+      System.out.println("The area is: " + area);
+    } else {
+      System.out.println("invalid input");
+    }
+
+  }
+
+}
